a43351596ab29828f9b3816dc781c7d645da86a4
andreikatata <andreevOandrei@gmail.com>
2020-06-30 18:25:57 +0100

    Small Rocket Changes

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -36,6 +36,11 @@ public class Rocket : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive)
+        {
+            return;
+        }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
